dff89f807107aa45167838829e20a3d318297a7d
muzimin <1367930183@qq.com>
2024-04-25 22:51:58 +0800

    feat: payment模块注册进入eureka服务

--- cloud-01-payment-8001/src/main/java/com/muzimin/PaymentMain01.java
+++ cloud-01-payment-8001/src/main/java/com/muzimin/PaymentMain01.java
@@ -2,12 +2,14 @@ package com.muzimin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author: 李煌民
  * @date: 2024-04-25 18:52
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain01 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain01.class, args);
