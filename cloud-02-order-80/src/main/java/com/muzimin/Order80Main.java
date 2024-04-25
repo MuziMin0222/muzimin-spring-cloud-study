d835befe695c672dfdb7b48941587aeeb2740b43
muzimin <1367930183@qq.com>
2024-04-25 22:35:47 +0800

    feat: order模块注册进入eureka服务

--- cloud-02-order-80/src/main/java/com/muzimin/Order80Main.java
+++ cloud-02-order-80/src/main/java/com/muzimin/Order80Main.java
@@ -2,12 +2,14 @@ package com.muzimin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author: 李煌民
  * @date: 2024-04-25 20:36
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class Order80Main {
     public static void main(String[] args) {
         SpringApplication.run(Order80Main.class, args);
