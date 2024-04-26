7dc6aaa5ba16ffe656122c5fbcdf191d1f8588e4
muzimin <1367930183@qq.com>
2024-04-26 14:34:32 +0800

    feat:restTemplate 在ribbon的其他用法

--- cloud-02-order-80/src/main/java/com/muzimin/controller/OrderController.java
+++ cloud-02-order-80/src/main/java/com/muzimin/controller/OrderController.java
@@ -2,7 +2,9 @@ package com.muzimin.controller;
 
 import com.muzimin.entity.CommonResult;
 import com.muzimin.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
  **/
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
     // String BaseUrl = "http://localhost:8001";
     // 改成微服务名称，每次访问都去eureka中拿地址，就回轮训拿，达到负载均衡效果
@@ -23,6 +26,17 @@ public class OrderController {
     @GetMapping("/payment/get/{id}")
     CommonResult selecyById(@PathVariable("id") long id) {
         return restTemplate.getForObject(BaseUrl + "/payment/select/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/payment2/get/{id}")
+    CommonResult selectById2(@PathVariable("id") long id) {
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(BaseUrl + "/payment/select/" + id, CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()) {
+            log.info("数据获取成功");
+            return forEntity.getBody();
+        }else {
+            return new CommonResult<>(400,"操作失败");
+        }
     }
 
     @GetMapping("/payment/create")
